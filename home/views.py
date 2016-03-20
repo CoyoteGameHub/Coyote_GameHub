5ab7a5c1bb7bd8007fe3b7615a9d5c0add9b1131
Lawrence Thai <thail@coyote.csusb.edu>
2016-03-19 23:12:46 -0700

    shit

--- home/views.py
+++ home/views.py
@@ -1,36 +1,44 @@
-from django.shortcuts import render
+from django.template.context_processors import csrf
+from django.shortcuts import render_to_response, render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 def index(request):
     return render(request, 'home/index.html')
 
 def login_and_auth(request, onsuccess='/userprofile/', onfail='/#login/'):
-    username = request.POST['username']
-    password = request.POST['password']
+    username = request.POST.get('username', False)
+    password = request.POST.get('password', False)
     user = authenticate(username=username, password=password)
     if user is not None:
-        if user.is_active:
-            login(request, user)
-            redirect(onsuccess)
-        else:
-            redirect(onfail)
+        # if user.is_active:
+        #     login(request, user)
+        #     return redirect(onsuccess)
+        # else:
+        #     return redirect(onfail)
+        login(request, user)
+        return redirect(onsuccess)
     else:
-        redirect(onfail)
+        return redirect(onfail)
 
-def signup_user(request):
+def signup_user(request, userexists="/#login/"):
     post = request.POST
-    if user_exists(post['email']): 
-        return redirect("/#login/")
-    else:
-        user = create_user(username=post['email'], email=post['email'], password=post['password'], name=post['name'])
-        return login_and_auth(request)
+    user = create_user(username=post['email'], email=post['email'], password=post['password'])
+    return login_and_auth(request)
+    # if not user_exists(post['email']): 
+    #     user = create_user(username=post['email'], email=post['email'], password=post['password'])
+    #     return login_and_auth(request)
+    # else:
+    #     return redirect(userexists)
+
 
 
 def user_exists(username):
-    user_count = User.objects.filter(username=username).count()
-    if user_count == 0:
-        return False
-    return True
+    if User.objects.filter(username=username).exists():
+        return True
+
+    return False
 
 def create_user(username, email, password):
     user = User(username=username, email=email)
