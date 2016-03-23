38f0f561a07c69ba84cf5a98fa2454bff4d51795
Lawrence Thai <thail@coyote.csusb.edu>
2016-03-22 17:25:14 -0700

    ghetto hack

--- Coyote_GameHub/settings.py
+++ Coyote_GameHub/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'Coyote_GameHub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'coyotegamehub',
-        'USER': 'coyotegamehub',
-        'PASSWORD': 'coyote92407',
-        'HOST': 'mysql.coyotegamehub.ml',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
